6e3e628690edc653aa4f55f08afd276835efb9ad
ivan006 <ivan.copeland2015@gmail.com>
2019-08-11 15:43:31 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <font-picker-el></font-picker-el>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1>Font Picker</h1>
+        <font-picker-el></font-picker-el>
+      </div>
+    </div>
   </div>
 </template>
 
